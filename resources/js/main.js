d803de034ed20fc4f96e2cba060304943c5bf555
braulioagr3 <braulioagr3@gmail.com>
2020-01-31 11:06:24 -0600

    Terminada la plantilla con funcionalidaad
    Ya funcionan las tabs solo falta darles estilo

---
+++ resources/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(
+    function()
+    {
+	$('.secciones article').hide(); //Se ocultan todos los articulos
+	$('.secciones article:first').show(); //Se muestra el primer articulo
+
+    $('ul.button li a').click//Se manda llamar al evento en caso de que se de click a un elemento de la lista
+    (
+        function()
+        {
+            var activeTab;
+            $('.secciones article').hide();//Se ocultan todos los articulos
+            activeTab = $(this).attr('href');//Se obtiene el id al que esta referenciado
+            $(activeTab).show();//Se muestra el articulo al cual se hace referencia        
+            return false;
+        }
+    );
+});
