c876d4491d3afde68ac1ba22abbfe6245be3903d
braulioagr3 <braulioagr3@gmail.com>
2020-02-04 11:33:44 -0600

    Mayor estilizado

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,13 +4,16 @@ $(document).ready
     {
         $('.secciones article').hide(); //Se ocultan todos los articulos
         $('.secciones article:first').show(); //Se muestra el primer articulo
-        $('ul.button li a').click//Se manda llamar al evento en caso de que se de click a un elemento de la lista
+        $('ul.menu li a:first').addClass('active');
+        $('ul.menu li a').click//Se manda llamar al evento en caso de que se de click a un elemento de la lista
         (
             function ()
             {
                 var activeTab;
                 $('.secciones article').hide();//Se ocultan todos los articulos
                 activeTab = $(this).attr('href');//Se obtiene el id al que esta referenciado
+                $('ul.menu li a').removeClass('active');
+                $(this).addClass('active');
                 $(activeTab).show();//Se muestra el articulo al cual se hace referencia        
                 return false;
             }
